21ed6e784824f623e022f3ce775baa897f4ed01b
senilaconstantin <constantin.senila@gmail.com>
2023-10-23 22:17:58 +0300

    release local

--- finding_clothes/lib/main.dart
+++ finding_clothes/lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'app/setup.dart';
+import 'src/app/setup.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- finding_clothes/test/widget_test.dart
+++ finding_clothes/test/widget_test.dart
@@ -9,7 +9,7 @@ import 'package:finding_clothes/src/app/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:finding_clothes/src/main.dart';
+import 'package:finding_clothes/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
