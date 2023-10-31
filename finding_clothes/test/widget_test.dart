206664cac5599e2d06d0b79311cf5c0e77a22c3e
senilaconstantin <constantin.senila@gmail.com>
2023-10-31 19:25:19 +0200

    delete main comment

--- finding_clothes/test/widget_test.dart
+++ finding_clothes/test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:finding_clothes/src/app/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:finding_clothes/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
