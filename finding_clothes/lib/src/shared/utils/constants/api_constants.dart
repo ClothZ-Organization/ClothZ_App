a0cf0565acf4694474d14ba011176420dd13d64a
senilaconstantin <constantin.senila@gmail.com>
2023-11-24 15:09:07 +0200

    add firestore

--- finding_clothes/lib/src/shared/utils/constants/api_constants.dart
+++ finding_clothes/lib/src/shared/utils/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
   static const String apiStart = "https://";
   static const String apiSearch = "serpapi.com/search.json?engine=google_lens&url={urlImg}&api_key={apiKey}";
-  static const String apiKey = "8972cdf5ece1b230f4cd01f70eea01f2b87fcbec8702d75d33cc0bd7758e543a";//'93a9b5ebdc416a158de5c31a02e6c1798adfada3ac0ed729bc53b3703650683c';//
+  static const String apiKey = '93a9b5ebdc416a158de5c31a02e6c1798adfada3ac0ed729bc53b3703650683c';//"8972cdf5ece1b230f4cd01f70eea01f2b87fcbec8702d75d33cc0bd7758e543a";//
 }
